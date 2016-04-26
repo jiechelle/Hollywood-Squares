0410a2998f0fa643e55c88c70351e9845186927e
Angelo Zamudio <avaz1301@gmail.com>
2016-04-25 21:27:19 -0400

    Created Game class, added checkWinner method.

---
+++ src/com/queens/Game.java
@@ -0,0 +1,37 @@
+package com.queens;
+
+import java.util.HashMap;
+
+/**
+ * Created by AngeloZamudio on 4/25/16.
+ */
+public class Game {
+    //Player [] players;
+    //Board board;
+    boolean winner;
+    HashMap questions;
+
+
+
+    public Game(/*Player []*/){
+
+
+
+    }//constructor
+
+    public void CheckWinner() {
+        //if(board.xCount==5){
+        //  user1.isWinner=true;
+        //}else if(board.oCount==5){
+        //  user2.isWinner=true;
+        //}
+        //
+
+    }//Check Winner
+
+    public void pickFirstPlayer(){
+
+    }
+
+
+}//Game
