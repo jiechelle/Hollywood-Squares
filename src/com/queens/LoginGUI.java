36d58d5b5bc63bf9c083c9710e793c4f528248cd
Alessandro DiMarco <axdimarco@gmail.com>
2016-05-07 16:50:41 -0400

    Update style and compare strings with equals

--- src/com/queens/LoginGUI.java
+++ src/com/queens/LoginGUI.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 
 public class LoginGUI {
 
-    private Scene loginScene;
+    // private Scene loginScene;
     private Stage loginStage;
     private Player[] players = new Player[2];
     static DataFile data;
@@ -31,19 +31,19 @@ public class LoginGUI {
     public void launchLogin(Stage stage) {
         loginStage = stage;
 
-        ///LOGIN PANEL FRAME CREATION
+        // LOGIN PANEL FRAME CREATION
         GridPane loginPane = new GridPane();
         loginPane.setAlignment(Pos.CENTER);
         loginPane.setHgap(10);
         loginPane.setVgap(10);
         loginPane.setPadding(new Insets(25, 25, 25, 25));
 
-        //LOGIN TITLE
+        // LOGIN TITLE
         Text scenetitle = new Text("Please Login or Register");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         loginPane.add(scenetitle, 0, 0, 2, 1);
 
-        //POP UP MESSAGES
+        // POP UP MESSAGES
         Alert loginAlert = new Alert(Alert.AlertType.WARNING);
         Alert selectAlert = new Alert(Alert.AlertType.CONFIRMATION);
         ButtonType buttonTypeOne = new ButtonType("One");
@@ -54,26 +54,26 @@ public class LoginGUI {
         selectAlert.setHeaderText("");
         selectAlert.setContentText("Select number of players: ");
 
-        //USER NAME BOX
+        // USER NAME BOX
         Label userName = new Label("Username:");
         loginPane.add(userName, 0, 1);
         TextField userField = new TextField();
         loginPane.add(userField, 1, 1);
 
-        //PASSWORD BOX
+        // PASSWORD BOX
         Label pw = new Label("Password:");
         loginPane.add(pw, 0, 2);
         PasswordField pwField = new PasswordField();
         loginPane.add(pwField, 1, 2);
 
-        //LOGIN BUTTON
+        // LOGIN BUTTON
         Button logBtn = new Button("Login");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(logBtn);
         loginPane.add(hbBtn, 1, 4);
 
-        //REGISTER BUTTON
+        // REGISTER BUTTON
         Button rBtn = new Button("Register");
         HBox hbBtn2 = new HBox(10);
         hbBtn2.setAlignment(Pos.BOTTOM_RIGHT);
@@ -81,20 +81,22 @@ public class LoginGUI {
         loginPane.add(hbBtn2, 1, 5);
 
 
-        //LOGIN button action
+        // LOGIN button action
         logBtn.setOnAction(e -> {
             String tempUser = userField.getText();
             String tempPass = pwField.getText();
 
             if (data.checkPlayerCredentials(tempUser, tempPass)) {
-                //assign players to array depending on case 1 or case 2
-                if (players[0] == null)
+
+                // assign players to array depending on case 1 or case 2
+                if (players[0] == null) {
                     players[0] = data.getPlayer(tempUser);
 
-                else if (players[0] != null && players[1] == null && tempUser != players[0].getUsername()) {
+                } else if (players[1] == null && !tempUser.equals(players[0].getUsername())) {
                     players[1] = data.getPlayer(tempUser);
+
                     //check if player 2 is duplicate player 1
-                    if (players[0].getUsername() == players[1].getUsername()) {
+                    if (players[0].getUsername().equals(players[1].getUsername())) {
                         loginAlert.setTitle("Error");
                         loginAlert.setHeaderText("You can't play against yourself, dummy");
                         loginAlert.setContentText("Make sure player two is a different account");
@@ -103,7 +105,7 @@ public class LoginGUI {
                     }
                 }
 
-                //TWO PLAYERS LOGGED IN, SWITCH STAGE TO GAME STAGE
+                // TWO PLAYERS LOGGED IN, SWITCH STAGE TO GAME STAGE
                 if (players[1] != null && players[0] != null) {
                     loginAlert.setTitle("Game Start");
                     loginAlert.setHeaderText("Two player game ");
@@ -113,10 +115,11 @@ public class LoginGUI {
                     gameGUI.launchGame(loginStage);
                 }
 
-                //PLayer 1 logged in successfully, BEGIN SELECTION SEQUENCE
+                // PLayer 1 logged in successfully, BEGIN SELECTION SEQUENCE
                 if (players[0] != null && players[1] == null) {
                     Optional<ButtonType> result = selectAlert.showAndWait();
-                    //START GAME WITH PLAYER 1 VS CPU
+
+                    // START GAME WITH PLAYER 1 VS CPU
                     if (result.get() == buttonTypeOne) {
                         loginAlert.setTitle("Game Start");
                         loginAlert.setHeaderText("One player game ");
@@ -126,13 +129,13 @@ public class LoginGUI {
                         gameGUI = new GameGUI(data, players);
                         gameGUI.launchGame(loginStage);
 
-                        //GO BACK TO LOGIN SCREEN AND REGISTER PLAYER 2
+                        // GO BACK TO LOGIN SCREEN AND REGISTER PLAYER 2
                     } else if (result.get() == buttonTypeTwo) {
                         userField.clear();
                         pwField.clear();
                         scenetitle.setText("Player 2 - Please Login");
 
-                        //USER PRESSED X OR CANCEL, reset player 0
+                        // USER PRESSED X OR CANCEL, reset player 0
                     } else {
                         players[0] = null;
                         pwField.clear();
@@ -147,7 +150,7 @@ public class LoginGUI {
             }
         });
 
-        //REGISTER button action
+        // REGISTER button action
         rBtn.setOnAction(e -> {
             String tempUser = userField.getText();
             String tempPass = pwField.getText();
@@ -156,16 +159,19 @@ public class LoginGUI {
                 data.addPlayer(tempUser,tempPass);
                 System.out.println("Account " + tempUser + " successfully created");
                 data.writePlayers();
+
             }catch(ValidationException e0){
                 loginAlert.setTitle("Registration Error");
                 loginAlert.setContentText("Text fields cannot have blank spaces");
                 loginAlert.showAndWait();
                 pwField.clear();
                 userField.clear();
+
             } catch(SecurityException e2){
                 loginAlert.setTitle("Registration Error");
                 loginAlert.setContentText("Text fields cannot be empty!");
                 loginAlert.showAndWait();
+
             } catch(Exception e1) {
                 loginAlert.setTitle("Registration Error");
                 loginAlert.setHeaderText("Username already taken");
@@ -176,7 +182,7 @@ public class LoginGUI {
             }
         });
 
-        loginScene = new Scene(loginPane, 300, 270);
+        Scene loginScene = new Scene(loginPane, 300, 270);
         loginStage.setTitle("Hollywood Squares");
         loginStage.setScene(loginScene);
         loginStage.setResizable(true);
